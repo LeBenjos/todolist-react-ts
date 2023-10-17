bda3713615849db43ada8c7bfbf8faa32a43d90d
Louisan Tchitoula <User11@MacBook-Pro.local>
2023-10-17 11:29:49 +0200

    feat(db): instantiate firestore db

--- src/App/context/firebase.tsx
+++ src/App/context/firebase.tsx
@@ -1,16 +1,19 @@
 // Firebase
 import { FirebaseApp, initializeApp } from "firebase/app";
+import { getFirestore } from "firebase/firestore";
 import { Auth, getAuth } from "firebase/auth";
 
 const firebaseConfig = {
-    apiKey: "AIzaSyCty8z4jHF3SUU9yrQWq4Dk7VHX2WbyI9o",
-    authDomain: "todolist-react-ts.firebaseapp.com",
-    projectId: "todolist-react-ts",
-    storageBucket: "todolist-react-ts.appspot.com",
-    messagingSenderId: "1062471774859",
-    appId: "1:1062471774859:web:bc1748d0c6656478e2da8e"
+  apiKey: "AIzaSyCty8z4jHF3SUU9yrQWq4Dk7VHX2WbyI9o",
+  authDomain: "todolist-react-ts.firebaseapp.com",
+  projectId: "todolist-react-ts",
+  storageBucket: "todolist-react-ts.appspot.com",
+  messagingSenderId: "1062471774859",
+  appId: "1:1062471774859:web:bc1748d0c6656478e2da8e",
 };
 
 const app: FirebaseApp = initializeApp(firebaseConfig);
 
 export const auth: Auth = getAuth(app);
+
+export const db = getFirestore(app);
