b1dc28cb62ab1c5855a0aba0d7853ae1f8114c7a
Louisan Tchitoula <t.louisan@outlook.com>
2023-10-17 15:46:42 +0200

    fix: style the auth state button

--- src/App/App.tsx
+++ src/App/App.tsx
@@ -1,28 +1,29 @@
 // Libraries
-import { Routes, Route } from 'react-router-dom';
+import { Routes, Route } from "react-router-dom";
 
 // Context
-import Auth from './context/Auth.tsx';
+import Auth from "./context/Auth.tsx";
 
 // Routes
-import PrivateRoute from './routes/PrivateRoute.tsx';
+import PrivateRoute from "./routes/PrivateRoute.tsx";
 
 // Pages
-import Authentication from './pages/Authentication.tsx';
-import Home from './pages/Home.tsx';
+import Authentication from "./pages/Authentication/Authentication.tsx";
+import Home from "./pages/Home.tsx";
 
 // Styles
-import './styles/app.css'
-import { PagePath } from './constants/paths/PagePath.ts';
+import "./styles/app.css";
+import { PagePath } from "./constants/paths/PagePath.ts";
 
 export default function App() {
-
-  return <Auth>
-    <Routes>
-      <Route path={ PagePath.AUTHENTIFICATION } element={<Authentication />} />
-      <Route element={<PrivateRoute redirect={ PagePath.AUTHENTIFICATION} /> }>
-        <Route path={ PagePath.HOME } element={<Home />} />
-      </Route>
-    </Routes>
-  </Auth>
+  return (
+    <Auth>
+      <Routes>
+        <Route path={PagePath.AUTHENTIFICATION} element={<Authentication />} />
+        <Route element={<PrivateRoute redirect={PagePath.AUTHENTIFICATION} />}>
+          <Route path={PagePath.HOME} element={<Home />} />
+        </Route>
+      </Routes>
+    </Auth>
+  );
 }
