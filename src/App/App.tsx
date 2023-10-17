d112e6c88674379c496f677a85963852380c5356
Louisan Tchitoula <t.louisan@outlook.com>
2023-10-17 16:12:59 +0200

    fix: authentication location

--- src/App/App.tsx
+++ src/App/App.tsx
@@ -8,14 +8,14 @@ import Auth from "./context/Auth.tsx";
 import PrivateRoute from "./routes/PrivateRoute.tsx";
 
 // Pages
-import Authentication from './pages/Authentication.tsx';
-import Home from './pages/Home.tsx';
+import Authentication from "./pages/Authentication/Authentication.tsx";
+import Home from "./pages/Home.tsx";
 
 // Constants
-import { PagePath } from './constants/paths/PagePath.ts';
+import { PagePath } from "./constants/paths/PagePath.ts";
 
 // Styles
-import './styles/app.css'
+import "./styles/app.css";
 
 export default function App() {
   return (
