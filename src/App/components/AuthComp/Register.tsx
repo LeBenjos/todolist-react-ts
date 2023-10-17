9a15529f6368342bfd281a240513fa60586e3b11
Louisan Tchitoula <t.louisan@outlook.com>
2023-10-17 13:48:29 +0200

    feat: auth register styling

--- src/App/components/AuthComp/Register.tsx
+++ src/App/components/AuthComp/Register.tsx
@@ -7,7 +7,14 @@ export default function Register() {
   const [password, setPassword] = useState("");
   const [username, setUsername] = useState("");
 
+  const [error, setError] = useState(false);
+
   const handleRegistration = async (e: any) => {
+    if (!email || !password || !username) {
+      setError(true);
+      return;
+    }
+
     e.preventDefault();
 
     await createUser({
@@ -18,41 +25,53 @@ export default function Register() {
   };
 
   return (
-    <form className="Register">
+    <form className="Auth" action="">
       <div className="input-container">
         <label htmlFor="name">Nom d'utilisateur</label>
         <input
+          className={error ? "error-input" : ""}
+          required
           type="text"
           name="name"
           id="name"
           value={username}
           onChange={(e) => {
+            setError(false);
             setUsername(e.target.value);
           }}
+          placeholder="Nom d'utilisateur"
         />
       </div>
       <div className="input-container">
         <label htmlFor="email">Adresse mail</label>
         <input
+          className={error ? "error-input" : ""}
+          required
           type="email"
           name="email"
           id="email"
           value={email}
           onChange={(e) => {
+            setError(false);
             setEmail(e.target.value);
           }}
+          placeholder="Adresse mail"
         />
       </div>
       <div className="input-container">
         <label htmlFor="password">Mot de passe</label>
         <input
+          className={error ? "error-input" : ""}
+          required
           type="password"
           name="password"
           id="password"
           value={password}
           onChange={(e) => {
+            setError(false);
             setPassword(e.target.value);
           }}
+          placeholder="Mot de passe"
         />
       </div>
 
