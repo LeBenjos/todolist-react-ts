0bc8c26f0bfafda5cb8f715dd1b1048eef9a6b7c
Louisan Tchitoula <t.louisan@outlook.com>
2023-10-17 11:42:42 +0200

    feat: set up states in register component

--- src/App/components/AuthComp/Register.tsx
+++ src/App/components/AuthComp/Register.tsx
@@ -1,23 +1,57 @@
-import React from "react";
+import React, { useState } from "react";
 import "./Auth.css";
 
 export default function Register() {
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+  const [username, setUsername] = useState("");
+
+  const handleRegistration = (e: any) => {
+    e.preventDefault();
+  };
+
   return (
-    <form>
+    <form className="Register">
       <div className="input-container">
         <label htmlFor="name">Nom d'utilisateur</label>
-        <input type="text" name="name" id="name" />
+        <input
+          type="text"
+          name="name"
+          id="name"
+          value={username}
+          onChange={(e) => {
+            setUsername(e.target.value);
+          }}
+        />
       </div>
       <div className="input-container">
         <label htmlFor="email">Adresse mail</label>
-        <input type="email" name="email" id="email" />
+        <input
+          type="email"
+          name="email"
+          id="email"
+          value={email}
+          onChange={(e) => {
+            setEmail(e.target.value);
+          }}
+        />
       </div>
       <div className="input-container">
         <label htmlFor="password">Mot de passe</label>
-        <input type="password" name="password" id="password" />
+        <input
+          type="password"
+          name="password"
+          id="password"
+          value={password}
+          onChange={(e) => {
+            setPassword(e.target.value);
+          }}
+        />
       </div>
 
-      <button>S'inscrire</button>
+      <button onClick={handleRegistration} type="submit">
+        S'inscrire
+      </button>
     </form>
   );
 }
