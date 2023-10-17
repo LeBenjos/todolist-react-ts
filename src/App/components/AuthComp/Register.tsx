f730b7c5557b444df45473321579c106a20500b5
Louisan Tchitoula <t.louisan@outlook.com>
2023-10-17 15:27:08 +0200

    fix: error handling for registration

--- src/App/components/AuthComp/Register.tsx
+++ src/App/components/AuthComp/Register.tsx
@@ -7,36 +7,54 @@ export default function Register() {
   const [password, setPassword] = useState("");
   const [username, setUsername] = useState("");
 
-  const [error, setError] = useState(false);
+  const [error, setError] = useState("");
+  const [success, setSuccess] = useState("");
 
   const handleRegistration = async (e: any) => {
     if (!email || !password || !username) {
-      setError(true);
       return;
     }
 
     e.preventDefault();
 
-    await createUser({
+    const createState = await createUser({
       email,
       password,
       username,
     });
+
+    createState
+      ? handleError(createState)
+      : handleSuccess("Compte crée avec succ");
+  };
+
+  const handleError = (text: string) => {
+    setSuccess("");
+    setError(text);
+  };
+
+  const handleSuccess = (text: string) => {
+    setError("");
+    setEmail("");
+    setPassword("");
+    setUsername("");
+    setSuccess(text);
   };
 
   return (
     <form className="Auth" action="">
+      <p className="error-input">{error}</p>
+      <p className="success-input">{success}</p>
       <div className="input-container">
         <label htmlFor="name">Nom d'utilisateur</label>
         <input
-          className={error ? "error-input" : ""}
           required
           type="text"
           name="name"
           id="name"
           value={username}
           onChange={(e) => {
-            setError(false);
+            setError("");
             setUsername(e.target.value);
           }}
           placeholder="Nom d'utilisateur"
@@ -45,14 +63,13 @@ export default function Register() {
       <div className="input-container">
         <label htmlFor="email">Adresse mail</label>
         <input
-          className={error ? "error-input" : ""}
           required
           type="email"
           name="email"
           id="email"
           value={email}
           onChange={(e) => {
-            setError(false);
+            setError("");
             setEmail(e.target.value);
           }}
           placeholder="Adresse mail"
@@ -61,14 +78,13 @@ export default function Register() {
       <div className="input-container">
         <label htmlFor="password">Mot de passe</label>
         <input
-          className={error ? "error-input" : ""}
           required
           type="password"
           name="password"
           id="password"
           value={password}
           onChange={(e) => {
-            setError(false);
+            setError("");
             setPassword(e.target.value);
           }}
           placeholder="Mot de passe"
