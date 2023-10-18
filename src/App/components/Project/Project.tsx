19ab9f11f0a1940de92aa86ae71f0400c5f4cc64
Louisan Tchitoula <t.louisan@outlook.fr>
2023-10-18 17:59:06 +0200

    feat: add state in project component for kanban data

--- src/App/components/Project/Project.tsx
+++ src/App/components/Project/Project.tsx
@@ -1,12 +1,13 @@
-import React, { useEffect } from "react";
+import React, { useEffect, useState } from "react";
 import { getKanbans } from "../../services/kanbans/getKanban.service";
 import { useParams } from "react-router-dom";
 
 export default function Project() {
+  const [kanbanData, setKanbanData] = useState<any>();
   const { id } = useParams();
   useEffect(() => {
     if (!id) return;
-    getKanbans(id);
+    setKanbanData(getKanbans(id));
   }, []);
   return <div>Project</div>;
 }
