e9355746db052f939bd907260e4a60cdcb6be3b6
Louisan Tchitoula <t.louisan@outlook.fr>
2023-10-18 23:49:34 +0200

    feat: fix param for creazte task

--- src/App/services/tasks/createTasks.service.ts
+++ src/App/services/tasks/createTasks.service.ts
@@ -3,9 +3,10 @@ import { db } from "../../context/firebase";
 
 export const createTask = async (
   projectId: string,
-  taskDescription: string
+  taskDescription: string,
+  state: string
 ) => {
-  const queryRef = doc(db, "projects", projectId, "kanban", "todo");
+  const queryRef = doc(db, "projects", projectId, "kanban", state);
   const resultSnap = await getDoc(queryRef);
 
   if (resultSnap.exists()) {
