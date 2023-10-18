dbc575d557e89fe3700096d0ec25b1b442982294
Louisan Tchitoula <t.louisan@outlook.fr>
2023-10-18 18:51:24 +0200

    feat: retrieve participants for getProjects

--- src/App/services/projects/getProjects.service.ts
+++ src/App/services/projects/getProjects.service.ts
@@ -1,17 +1,35 @@
-import { collection, doc, getDoc, getDocs } from "firebase/firestore";
+import {
+  collection,
+  doc,
+  getDoc,
+  getDocs,
+  query,
+  where,
+} from "firebase/firestore";
 import { db } from "../../context/firebase";
 
 export const getProjects = async (userId: string) => {
   const queryRef = collection(db, "users", userId, "projects");
   const resultSnap = await getDocs(queryRef);
 
-  const projectList: any = [];
+  const projectList: any[] = [];
 
   resultSnap.forEach((doc) => {
-console.log(doc);
+    console.log(doc);
     projectList.push({ ...doc.data(), id: doc.id });
   });
-//   console.log(bloc.exists() ? bloc.data() : "non");
+
+  const sharedProjectsRef = query(
+    collection(db, "projects"),
+    where("participants", "array-contains", userId)
+  );
+  const resultSharedProjectsSnap = await getDocs(queryRef);
+
+  resultSharedProjectsSnap.forEach((project) => {
+    projectList.push({ ...project.data(), id: project.id });
+  });
+
+  //   console.log(bloc.exists() ? bloc.data() : "non");
   //   return bloc.exists() ? bloc.data() : {};
 
   return projectList;
