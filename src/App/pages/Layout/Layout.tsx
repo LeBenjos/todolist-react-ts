b4188257ccb2a373550f1ff3d2fa385449a61440
LeBenjos <benjamin.schinkel01@outlook.fr>
2023-10-18 15:14:51 +0200

    style: Layout

--- src/App/pages/Layout/Layout.tsx
+++ src/App/pages/Layout/Layout.tsx
@@ -1,18 +1,19 @@
+// Libraries
 import { Outlet } from "react-router-dom";
 
+// Styles
 import "./Layout.css";
 
-import SideBar from "../../components/SideBarComp/SideBar";
-import Header from "../../components/Header/Header";
+// Components
+import SideBar from "../../components/SideBarComp/SideBar.tsx";
+import Header from "../../components/Header/Header.tsx";
 
 export default function Layout() {
-  return (
-    <div className="Layout">
-      <SideBar />
-      <div className="Main">
-        <Header />
-        <Outlet />
-      </div>
+  return <div className="Layout">
+    <SideBar />
+    <div className="Main">
+      <Header />
+      <Outlet />
     </div>
-  );
+  </div>
 }
