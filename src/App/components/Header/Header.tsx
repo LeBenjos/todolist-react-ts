5af94ea36c78aa1679ed0eaefa10b2137e7e8af3
Louisan Tchitoula <t.louisan@outlook.fr>
2023-10-19 03:07:36 +0200

    fix: header snapshot

--- src/App/components/Header/Header.tsx
+++ src/App/components/Header/Header.tsx
@@ -17,32 +17,48 @@ import HeaderLeft from "./Side/HeaderLeft.tsx";
 import HeaderRight from "./Side/HeaderRight.tsx";
 
 // Styles
-import "./header.css"
+import "./header.css";
 import { compareArraysOfObjects } from "../../services/functions/CompareArraysOfObjects.ts";
+import { onSnapshot, query, collection, where } from "firebase/firestore";
+import { db } from "../../context/firebase.tsx";
 
 export default function Header() {
-    const [userData, setUserData] = useState<User | undefined>();
-    const [projects, setProjects] = useState<Project[]>();
-    const user = useContext(authContext);
+  const [userData, setUserData] = useState<User | undefined>();
+  const [projects, setProjects] = useState<Project[]>();
+  const user = useContext(authContext);
 
-    useEffect(() => {
-        if (!user) return;
-        get(user.providerData[0].uid).then((res) => setUserData(res as User));
-        getProjects(user.providerData[0].uid).then((res) => setProjects(res as Project[]));
-    }, []);
+  useEffect(() => {
+    if (!user) return;
+    get(user.providerData[0].uid).then((res) => setUserData(res as User));
 
-    useEffect(() => {
-        if (!user) return;
-        getProjects(user.providerData[0].uid)
-            .then((res) => {
-                if(projects && compareArraysOfObjects(res, projects as Project[])) return;
-                setProjects(res as Project[])
-            });
-    }, [projects]);
+    onSnapshot(
+      query(
+        collection(db, "projects"),
+        where("participants", "array-contains", user.providerData[0].uid)
+      ),
+      (snapshot) => {
+        const result: any[] = [];
+        snapshot.docs.forEach((doc) => {
+          result.push({ ...doc.data(), id: doc.id });
+        });
+        setProjects([...result]);
+      }
+    );
+  }, []);
 
+  useEffect(() => {
+    if (!user) return;
+    getProjects(user.providerData[0].uid).then((res) => {
+      if (projects && compareArraysOfObjects(res, projects as Project[]))
+        return;
+      setProjects(res as Project[]);
+    });
+  }, [projects]);
 
-    return <div className="header-container">
-        <HeaderLeft userData={userData} projects={projects} />
-        <HeaderRight userData={userData} projects={projects} />
+  return (
+    <div className="header-container">
+      <HeaderLeft userData={userData} projects={projects} />
+      <HeaderRight userData={userData} projects={projects} />
     </div>
+  );
 }
