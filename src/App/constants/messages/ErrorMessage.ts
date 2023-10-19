a9e38cc7ef2cf6c97e0e747342621c58e7476176
LeBenjos <benjamin.schinkel01@outlook.fr>
2023-10-19 04:09:37 +0200

    feat: messages add member

--- src/App/constants/messages/ErrorMessage.ts
+++ src/App/constants/messages/ErrorMessage.ts
@@ -4,4 +4,7 @@ export enum ErrorMessage {
     AUTH_INVALID_LOGIN_CREDENTIALS = "Mot de passe incorrect",
     AUTH_INVALID_INPUT = "Champ obligatoire manquant",
     AUTH_DEFAULT_MESSAGE = "Problème lors de l'authentification",
+
+    // ADD_MEMBER
+    MEMBER_NOT_FOUND = "L'email n'est associé à aucun compte",
 }
