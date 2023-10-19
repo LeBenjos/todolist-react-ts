f816f3754d9830e296956ba9a22b7f8497639993
Louisan Tchitoula <t.louisan@outlook.fr>
2023-10-19 03:30:04 +0200

    feat: project listener

--- src/App/components/Project/Project.tsx
+++ src/App/components/Project/Project.tsx
@@ -7,19 +7,27 @@ import { authContext } from "../../context/Auth";
 import Kanban from "./Kanban/Kanban";
 import "./Project.css";
 import { KanbanText } from "../../constants/texts/KanbanText";
+import { collection, doc, onSnapshot } from "firebase/firestore";
+import { db } from "../../context/firebase";
 
 export default function Project() {
   const [kanbanData, setKanbanData] = useState<any[]>([]);
   const { id } = useParams();
   const user = useContext(authContext);
+
   useEffect(() => {
     if (!id) return;
-    getKanbans(id).then((res) => {
-      console.log(res);
-      setKanbanData(res);
+
+    onSnapshot(collection(db, "projects", id, "kanban"), (snapshot) => {
+      const result: any[] = [];
+      snapshot.docs.forEach((doc) => {
+        console.log(doc.data(), doc.id);
+        result.push({ state: doc.id, ...doc.data() });
+      });
+      setKanbanData(result);
     });
     // createTask(id, "tache");
-  }, []);
+  }, [id]);
 
   return (
     <div className="Project">
@@ -27,17 +35,23 @@ export default function Project() {
         <div className="kanban-layout">
           <Kanban
             state={KanbanText.TODO}
-            tasks={kanbanData.find((kanban) => kanban.state === KanbanText.TODO).tasks}
+            tasks={
+              kanbanData.find((kanban) => kanban.state === KanbanText.TODO)
+                .tasks
+            }
             text={KanbanText.TODO_TEXT}
           />
           <Kanban
             state={KanbanText.DOING}
-            tasks={kanbanData.find((kanban) => kanban.state === KanbanText.DOING).tasks}
+            tasks={
+              kanbanData.find((kanban) => kanban.state === KanbanText.DOING)
+                .tasks
+            }
             text={KanbanText.DOING_TEXT}
           />
           <Kanban
             state={KanbanText.DONE}
-            tasks={kanbanData.find((kanban) => kanban.state === KanbanText.DONE).tasks}
+            tasks={kanbanData.find((kanban) => kanban.state == "done").tasks}
             text={KanbanText.DONE_TEXT}
           />
         </div>
